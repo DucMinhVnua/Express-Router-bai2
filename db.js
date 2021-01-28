bd4585d3a797c8fb194895c8b7922514f7ed4df0
unknown <ndminhvnua@gmail.com>
2021-01-28 12:59:02 +0700

    bai2

---
+++ db.js
@@ -0,0 +1,10 @@
+// lowdb
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+const { render } = require('pug');
+const adapter = new FileSync('database.json');
+const db = low(adapter);
+db.defaults({ books: [] }).write(); // đoạn này để set default trong file json ta có một mạng posts rỗng
+db.defaults({ users: [] }).write();
+
+module.exports = db;
